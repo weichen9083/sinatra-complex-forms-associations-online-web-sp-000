e3ff806e565a3382125c775abc5b386864482ab2
wei chen <weichen9083@gmail.com>
2021-06-24 04:48:46 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -12,10 +12,8 @@ class PetsController < ApplicationController
 
   post '/pets' do
     @pet = Pet.create(params[:pet])
-    if !params[:owner][:name].empty?
+    if !params["owner"]["name"].empty?
       @pet.owner = Owner.create(name: params[:owner][:name])
-    else
-      @pet.owner = Owner.find_by_id(params[:pet][:owner_id])
     end
     
     redirect to "pets/#{@pet.id}"
