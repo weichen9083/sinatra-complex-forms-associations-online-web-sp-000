7a0c84b8417605519449678706f26a8bbe43f5b8
wei chen <weichen9083@gmail.com>
2021-06-24 04:46:39 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -17,7 +17,7 @@ class PetsController < ApplicationController
     else
       @pet.owner = Owner.find_by_id(params[:pet][:owner_id])
     end
-    @pet.save
+    
     redirect to "pets/#{@pet.id}"
   end
   
